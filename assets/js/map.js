8929ee938c732561ca7ed4b28b171e3107accb2f
evan <frawleyevan@gmail.com>
2026-02-07 15:13:11 -0600

    Add map to Maps page

--- assets/js/map.js
+++ assets/js/map.js
@@ -14,7 +14,10 @@
   `;
 
   if (typeof L === "undefined") {
-const DefaultIcon = L.icon({
+    return;
+  }
+
+  const DefaultIcon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
@@ -22,8 +25,8 @@ const DefaultIcon = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = DefaultIcon;
+  });
+  L.Marker.prototype.options.icon = DefaultIcon;
 
 
   const map = L.map("map", {
